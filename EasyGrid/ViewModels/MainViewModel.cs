35d2ffd1378deb0dd042c363ac5469ff94be752b
Leonid Boginski <65702576+leonidbogin@users.noreply.github.com>
2021-08-01 05:00:59 +0300

    Add UI prototype part

---
+++ EasyGrid/ViewModels/MainViewModel.cs
@@ -0,0 +1,134 @@
+﻿using DevExpress.Mvvm;
+using EasyGrid.Commands;
+using System.Collections.Generic;
+using System.Windows.Input;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace EasyGrid.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private readonly SASLastSelectionProvider lastSelectionProvider;
+
+        private double leftTopLat;
+        public double LeftTopLat
+        {
+            get => leftTopLat;
+            set
+            {
+                leftTopLat = value;
+                RaisePropertyChanged(() => LeftTopLat);
+            }
+        }
+
+        private double leftTopLon;
+        public double LeftTopLon
+        {
+            get => leftTopLon;
+            set
+            {
+                leftTopLon = value;
+                RaisePropertyChanged(() => LeftTopLon);
+            }
+        }
+
+        private double rightBottomLat;
+        public double RightBottomLat
+        {
+            get => rightBottomLat;
+            set
+            {
+                rightBottomLat = value;
+                RaisePropertyChanged(() => RightBottomLat);
+            }
+        }
+
+        private double rightBottomLon;
+        public double RightBottomLon
+        {
+            get => rightBottomLon;
+            set
+            {
+                rightBottomLon = value;
+                RaisePropertyChanged(() => RightBottomLon);
+            }
+        }
+
+        private int squareSize;
+        public int SquareSize
+        {
+            get => squareSize;
+            set
+            {
+                squareSize = value;
+                RaisePropertyChanged(() => SquareSize);
+            }
+        }
+
+        private IEnumerable<int> squareSizes;
+        public IEnumerable<int> SquareSizes
+        {
+            get => squareSizes;
+            set
+            {
+                squareSizes = value;
+                RaisePropertyChanged(() => SquareSizes);
+            }
+        }
+
+        public MainViewModel() 
+        {
+            var sizes = new int[] { 500, 250, 200, 100 };
+            squareSizes = sizes;
+            squareSize = sizes[0];
+
+            leftTopLat = 55.177905;
+            leftTopLon = 30.203977;
+
+            rightBottomLat = 55.172728;
+            rightBottomLon = 30.208190;
+
+            lastSelectionProvider = new SASLastSelectionProvider();
+        }
+
+        public ICommand CreateGridCommand
+        {
+            get
+            {
+                return new CreateGridCommand((obj) =>
+                {
+                    var createGridProvider = new CreateGridProvider(leftTopLat, leftTopLon, rightBottomLat, rightBottomLon, squareSize);
+                    var convertToGpxProvider = new ConvertToGpxProvider();
+
+                    var grid = createGridProvider.CreateGrid();
+                    var gpx = convertToGpxProvider.ConvertToGpx(grid);
+
+                    var xmlWriterSettings = new XmlWriterSettings { Indent = true };
+                    using (var writer = XmlWriter.Create("filepath.xml", xmlWriterSettings))
+                    {
+                        var serializer = new XmlSerializer(typeof(Gpx));
+                        serializer.Serialize(writer, gpx);
+                    }
+                });
+            }
+        }
+
+        public ICommand CopyCoordinatesCommand
+        {
+            get
+            {
+                return new CopyCoordinatesCommand((obj) =>
+                {
+                    var points = lastSelectionProvider.GetLastSelection();
+
+                    LeftTopLat = points[0].Lat;
+                    LeftTopLon = points[0].Lon;
+
+                    RightBottomLat = points[1].Lat;
+                    RightBottomLon = points[1].Lon;
+                });
+            }
+        }
+    }
+}
