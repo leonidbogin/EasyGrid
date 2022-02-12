3cdeb2c74d695c37be441ffb5a2d534a48f9da5d
Leonid Boginski <65702576+leonidbogin@users.noreply.github.com>
2022-02-13 00:31:06 +0300

    Fix window DragMove (remove custom logic)

--- EasyGrid/Views/MainWindow.xaml.cs
+++ EasyGrid/Views/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using System.Windows;
-using System.Windows.Input;
 
 namespace EasyGrid.Views
 {
@@ -8,54 +7,6 @@ namespace EasyGrid.Views
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            if (WindowState != WindowState.Maximized && e.ChangedButton == MouseButton.Left)
-            {
-                DragMove();
-            }
-        }
-
-        private bool _restoreIfMove = false;
-        private void Title_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            if (e.ClickCount >= 2)
-            {
-                WindowState = WindowState switch
-                {
-                    WindowState.Normal => WindowState.Maximized,
-                    WindowState.Maximized => WindowState.Normal,
-                    _ => WindowState
-                };
-            } 
-            else if (WindowState == WindowState.Maximized)
-            {
-                if (e.ChangedButton == MouseButton.Left)
-                {
-                    _restoreIfMove = true;
-                    DragMove();
-                }
-            }
-        }
-
-        private void Window_MouseUp(object sender, MouseButtonEventArgs e)
-        {
-            _restoreIfMove = false;
-        }
-
-        private void Window_MouseMove(object sender, MouseEventArgs e)
-        {
-            if (_restoreIfMove)
-            {
-                _restoreIfMove = false;
-                var point = PointToScreen(e.MouseDevice.GetPosition(this));
-                Left = point.X - (RestoreBounds.Width * 0.5);
-                Top = point.Y;
-                WindowState = WindowState.Normal;
-                DragMove();
-            }
         }
     }
 }
