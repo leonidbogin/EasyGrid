a4257e905394de1d9c65107d10726d2c1003df2f
Leonid Boginski <65702576+leonidbogin@users.noreply.github.com>
2022-02-14 18:36:21 +0300

    Add SASPlanetSelection (Core.Models); Add SASPlanetSelectionProvider (Core.Providers)

---
+++ EasyGrid.Core/Models/SASPlanetSelection.cs
@@ -0,0 +1,17 @@
+﻿namespace EasyGrid.Core.Models
+{
+    public class SASPlanetSelection
+    {
+        public int Zoom { get; set; }
+        public double PointLon1 { get; set; }
+        public double PointLat1 { get; set; }
+        public double PointLon2 { get; set; }
+        public double PointLat2 { get; set; }
+        public double PointLon3 { get; set; }
+        public double PointLat3 { get; set; }
+        public double PointLon4 { get; set; }
+        public double PointLat4 { get; set; }
+        public double PointLon5 { get; set; }
+        public double PointLat5 { get; set; }
+    }
+}
---
+++ EasyGrid.Core/Providers/SASPlanetSelectionProvider.cs
@@ -0,0 +1,54 @@
+﻿using EasyGrid.Core.Models;
+using IniParser;
+using System;
+using System.IO;
+
+namespace EasyGrid.Core.Providers
+{
+    public class SASPlanetSelectionProvider
+    {
+        private readonly FileIniDataParser _parser;
+        private readonly string _path;
+
+        public SASPlanetSelectionProvider(string path)
+        {
+            _parser = new FileIniDataParser();
+            _path = path;
+        }
+
+        public SASPlanetSelection GetSelection()
+        {
+            if (!File.Exists(_path))
+            {
+                throw new FileNotFoundException($"{_path} file not found");
+            }
+
+            var data = _parser.ReadFile(_path);
+
+            return new SASPlanetSelection()
+            {
+                Zoom = Parse<int>(data["HIGHLIGHTING"]["PointLon_1"]),
+                PointLon1 = Parse<double>(data["HIGHLIGHTING"]["PointLon_1"]),
+                PointLat1 = Parse<double>(data["HIGHLIGHTING"]["PointLat_1"]),
+                PointLon2 = Parse<double>(data["HIGHLIGHTING"]["PointLon_2"]),
+                PointLat2 = Parse<double>(data["HIGHLIGHTING"]["PointLat_2"]),
+                PointLon3 = Parse<double>(data["HIGHLIGHTING"]["PointLon_3"]),
+                PointLat3 = Parse<double>(data["HIGHLIGHTING"]["PointLat_3"]),
+                PointLon4 = Parse<double>(data["HIGHLIGHTING"]["PointLon_4"]),
+                PointLat4 = Parse<double>(data["HIGHLIGHTING"]["PointLat_4"]),
+                PointLon5 = Parse<double>(data["HIGHLIGHTING"]["PointLon_5"]),
+                PointLat5 = Parse<double>(data["HIGHLIGHTING"]["PointLat_5"]),
+            };
+        }
+
+        public bool SelectionFileExists()
+        {
+            return File.Exists(_path);
+        }
+
+        private static T Parse<T>(string value)
+        {
+            return (T)Convert.ChangeType(value, typeof(T));
+        }
+    }
+}
